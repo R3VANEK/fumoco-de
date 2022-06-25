b5b09e41cc3906ce23caa00d4dd785d30795238b
Jan Napieralski <jan.napieralski.torun@wp.pl>
2022-06-25 07:42:32 +0200

    first desktop contact section styling

--- res/js/banner.js
+++ res/js/banner.js
@@ -1,13 +1,15 @@
 
 
-window.addEventListener('load', function(){
-    new Glide('.glide',{
-        type:'carousel',
-        autoplay: 3000,
-        hoverpause: true,
-        animationDuration : 2000,
-        keyboard: true,
-        AnimationTimingFunc: "ease-in",
-        hoverpause: false
-    }).mount()
-  })
+// window.addEventListener('load', function(){
+//     new Glide('.glide',{
+//         type:'carousel',
+//         autoplay: 3000,
+//         hoverpause: true,
+//         animationDuration : 2000,
+//         keyboard: true,
+//         AnimationTimingFunc: "ease-in",
+//         hoverpause: false
+//     }).mount()
+
+//   })
+
