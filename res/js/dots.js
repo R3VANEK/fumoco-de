d3e6b0490fa9a007a514554907a1bfe5d262f7d1
R3VANEK <janek2000-2000@wp.pl>
2022-07-02 23:21:08 +0200

    about us styling

--- res/js/dots.js
+++ res/js/dots.js
@@ -1,11 +1,27 @@
 const sections = $("section");
 const windowHeight = window.innerHeight;
 
+
+/**
+ * @type boolean[]
+ */
+let animationCheck = [];
+
 document.addEventListener("DOMContentLoaded", function(){
+
+    $("[data-scroll-animation]").each((i,e)=>{
+        $(e).css("opacity", "0")
+
+    })
+    $("[data-scroll-animation^=slide]").each((i,e)=>{
+        $(e).css("transform", "translateX(-50%)")
+    })
+    
 
     $("section").each((i, e)=>{
         let id = $(e).attr("id");
         $("#scroll-dots-holder > ul").append(`<li class="scroll-dot"><a href="#${id}" data-section-number=${i} rel="nofollow">link</a></li>`)
+        animationCheck.push(false);
     })
 
     $("#scroll-dots-holder > ul li:first-child").attr("section-scrolled-active", "true")
@@ -21,7 +37,54 @@ document.getElementById("fullpage-wrapper").addEventListener("scroll", function(
         ){
             $(".scroll-dot").removeAttr("section-scrolled-active");
             $(".scroll-dot").eq(i).attr("section-scrolled-active", "true");
+
+            scrollAnimations($("section").eq(i), i);
         }
     })
 
 })
+
+
+const scrollAnimations = (parent, index) =>{
+    
+    if(animationCheck[index])
+        return
+
+    let elementsToAnimate = parent.find("[data-scroll-animation]");
+
+
+    elementsToAnimate.each((i, e)=>{
+
+        let animationType = $(e).attr("data-scroll-animation");
+        let animationTiming = parseInt($(e).attr("data-scroll-timing"));
+        if(animationTiming === undefined || animationTiming === null || animationTiming <= 0)
+            animationTiming = 500;
+
+
+        if(animationType.includes("fade")){
+            $(e).css({
+                "transitionDuration": animationTiming+"ms",
+                "opacity": "1",
+            })
+        }
+
+        else if(animationType.includes("slide")){
+            $(e).css({
+                "transitionDuration": animationTiming+"ms",
+                "opacity": "1",
+                "transform": "translateX(0%)"
+            })
+        }
+
+        else{
+            $(e).css({
+                "transitionDuration": animationTiming+"ms",
+                "opacity": "1",
+            })
+        }
+
+    })
+
+    
+    animationCheck[index] = true;
+}
